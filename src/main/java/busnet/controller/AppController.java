fb8f9f55ecfe5f408202963fc509279dd54ebe77
Gaindalf <efremovaleksandr123@gmail.com>
2016-12-11 00:01:33 +0300

    Working version with Spring Security

---
+++ src/main/java/busnet/controller/AppController.java
@@ -0,0 +1,32 @@
+package busnet.controller;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AppController {
+    @RequestMapping("/")
+    public String hello(Model model) {
+        model.addAttribute("hello", "Hello World");
+        return "hello";
+    }
+
+    @RequestMapping("/confidential/page")
+    public String secureTable(Model model){
+        model.addAttribute("secure", "Secure page");
+        return "secure";
+    }
+
+    @RequestMapping(value = {"hello/{name}"}, method = RequestMethod.GET)
+    public ModelAndView hello(@PathVariable("name") String name) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("encode");
+        modelAndView.addObject("crypt", new BCryptPasswordEncoder().encode(name));
+        return modelAndView;
+    }
+}
---
+++ web/WEB-INF/views/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Aleksandr
+  Date: 10.12.2016
+  Time: 16:00
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title> Hello World</title>
+</head>
+<body>
+${hello}
+</body>
+</html>
