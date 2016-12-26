aaa8df8799b46bdbd2c4361ed8497a1df141dc2b
Gaindalf <efremovaleksandr123@gmail.com>
2016-12-26 22:57:08 +0300

    Add all back-end.

--- src/main/java/busnet/dao/ScheduleDao.java
+++ src/main/java/busnet/dao/ScheduleDao.java
@@ -6,19 +6,33 @@ import java.util.List;
 
 public interface ScheduleDao {
     public void add(Schedule schedule);
+
     public void edit(Schedule schedule);
+
     public void delete(int id);
+
     public Schedule getSchedule(int id);
+
     public List getAllSchedule();
+
     public Schedule getById(Integer id);
+
     public List<Schedule> getAll();
+
     public List<Schedule> getAllByStation(String station);
+
     public List<Schedule> getAllStation();
+
     public List<Schedule> getAllByOne();
+
     public List<Schedule> getDirectionByStation(String station);
+
     public Schedule getIdByName(String station);
+
     public int getStationNumber(String station);
+
     public List getStationByStationAndDirection(int a, int b, boolean direction);
+
     public int chooseRunNumber(String station, String time, boolean direction);
 
 }
--- src/main/java/busnet/service/RegistrationService.java
+++ src/main/java/busnet/service/RegistrationService.java
@@ -6,9 +6,14 @@ import java.util.List;
 
 public interface RegistrationService {
     public void add(Users users);
+
     public void edit(Users users);
+
     public void delete(int id);
+
     public Users getUser(int id);
+
     public List getAllUsers();
+
     public String getUserName(String name);
 }
